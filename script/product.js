bcf9946c1c0ff7d4b3f7edd29519904e858f6c63
UnathiGcetywa80 <unathigcetywa80@gmail.com>
2023-12-05 13:49:10 +0200

    changes in products

--- script/product.js
+++ script/product.js
@@ -1,1 +1,24 @@
 document.querySelector("#currYear").textContent = new Date().getFullYear()
+
+// let products = JSON.parse(localStorage.getItem('product')) {
+//     name: 'product1',
+//     image: 'https://i.postimg.cc/x8b94tHT/ladies-sneaker3.webp',
+//     description: 'Ladies Blue Sneakers'
+// }
+// const products = [
+// {id: 1, name:'product1', price: 2900.00},
+// {id: 1, name:'product1', price: 2900.00},
+// {id: 1, name:'product1', price: 2900.00},
+// {id: 1, name:'product1', price: 2900.00},
+// {id: 1, name:'product1', price: 2900.00},
+// {id: 1, name:'product1', price: 2900.00},
+// ];
+// function displayProducts(productArray) {
+//     const productList = document.getElementById('productlist').innerHTML = 'productlist';
+
+
+//     productArray.forEach(product => {
+//         const productItem = document.createElement('div');
+        
+//     })
+// } 
