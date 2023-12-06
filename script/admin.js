600fcb8a09846d9dc562243a0ca3094f34885fae
UnathiGcetywa80 <unathigcetywa80@gmail.com>
2023-12-06 13:49:04 +0200

    product.js

--- script/admin.js
+++ script/admin.js
@@ -1,4 +1,24 @@
 //edit your admin javascript on this file
 document.querySelector("#currYear").textContent = new Date().getFullYear()
 
-let products = JSON.parse(localStorage.getItem(''))
+let table = document.querySelector('table');
+
+function displayProducts(products){
+    let items = products.map(function (products, index) {
+        console.log(products);
+        console.log(index);
+        return`
+        <tr>
+        <td>${products.id}</td>
+        <td>${products.name}</td>
+        <td>${products.description}</td>
+        <td>R${products.price}</td>
+        <td><img src ="${products.url}"></td>
+        <td><button class='add' value='${index}'>Add to cart</button></td>
+        </tr>
+        `
+    })
+
+    table.innerHTML = items.join('')
+}
+displayProducts(products)
